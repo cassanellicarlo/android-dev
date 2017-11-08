65b497436576315f80cf97a64e132d07d4affc22
cassanellicarlo <cassatek@gmail.com>
2017-11-08 14:23:25 +0100

    aggiunta rilevazione della posizione utente

--- Map/app/src/main/java/com/example/carlo/map/MainActivity.java
+++ Map/app/src/main/java/com/example/carlo/map/MainActivity.java
@@ -1,29 +1,52 @@
 package com.example.carlo.map;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, LocationListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
 
     private ArrayList<Utente> utenti;
     private GoogleMap mMap = null;
+    private static final int LOCATION_PERMISSION = 1;
+    private GoogleApiClient mGoogleApiClient=null;
+    private TextView myTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Testo per messaggio sui permessi (concessi o meno)
+        myTextView = (TextView)findViewById(R.id.messaggio);
 
         // Get the SupportMapFragment and request notification
         // when the map is ready to be used.
@@ -32,6 +55,16 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mapFragment.getMapAsync(this);
 
         inserisciUtenti();
+    }
+
+    protected void onStart() {
+        if(mGoogleApiClient!=null) mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    protected void onStop() {
+        if(mGoogleApiClient!=null) mGoogleApiClient.disconnect();
+        super.onStop();
     }
 
     // Inserisco 5 Utenti con Nome, Latitutine e Longitudine
@@ -81,18 +114,19 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     }
 
-    // Quando la mappa è pronta abilito il bottone e chiamo la funzione addMarkers()
+    // Quando la mappa è pronta abilito i bottoni e inizializzo la mappa
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
         mMap=googleMap;
 
-        // Abilito il bottone 'Mostra'
+        // Abilito il bottone 'Mostra utenti' e 'La mia posizione'
         findViewById(R.id.bottone).setEnabled(true);
+        findViewById(R.id.bottone2).setEnabled(true);
 
     }
 
-    // Aggiungo i marker nella mappa
+    // Aggiungo i marker nella mappa --> PRIMO BOTTONE: 'Mostra Utenti'
     public void addMarkers(View view) {
 
         // per ogni utente aggiungio un Marker con Lat e Lon e come titolo il nome utente
@@ -105,7 +139,130 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         // Calcolo la mia area per mostrare tutti i marker
         LatLngBounds myBounds=myArea();
 
+        // Centro la mappa sull'area che ho calcolato
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myBounds.getCenter(),0));
     }
 
+    // Click del bottone 'La mia posizione' --> Chiedo i permessi all'utente per
+    // mostrare la sua posizione
+    public void myPosition(View view) {
+
+        // Controllo Permessi per Location
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+
+            Log.d("Main Activity", "Permission granted!");
+            myTextView.setText("Permessi per mostrare la tua posizione ottenuti. Posizione mostrata sulla mappa");
+            startGoogleApiClient();
+
+
+        } else{
+
+            Log.d("Main Activity", "Not Granted yet.");
+
+            // Richiesta dei permessi
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    LOCATION_PERMISSION);
+        }
+
+
+    }
+
+    public void startGoogleApiClient (){
+
+        // Creazione di un'istanza di GoogleAPIClient: permette di accedere
+        // ai servizi di Google Play Services
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+
+            mGoogleApiClient.connect();
+        }
+
+
+    }
+
+    // Metodo chiamato quando il Google Api Client è pronto
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.d("onConnected:", "Google API Client Ready!");
+
+        // Setto i parametri per l'acquisizione della posizione
+        LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+
+        }
+    }
+
+    // Il metodo onLocationChanged viene richiamato quando c’è una
+    // nuova posizione, passando come argomento la posizione stessa.
+    // Aggiungo un marker per la posizione dell'utente e centro la mappa in quella posizione
+    @Override
+    public void onLocationChanged(Location location) {
+        LatLng myPosition=new LatLng(location.getLatitude(),location.getLongitude());
+
+        // Aggiungo il marker settando i parametri
+        mMap.addMarker(new MarkerOptions()
+                .position(myPosition)
+                .title("La mia posizione")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+
+        // Centro la mappa sulla mia posizione
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(myPosition));
+
+        // Zommo la mappa con un'animazione
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d("onConnectionSuspended", "Connessione sospesa Google API Client");
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d("onConnectionFailed", "Connessione fallita Google API Client");
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case LOCATION_PERMISSION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    startGoogleApiClient();
+
+                    // Permessi Garantiti
+                    Log.d("Main Activity", "Permessi OK");
+                    myTextView.setText("Permessi per mostrare la tua posizione ottenuti. Posizione mostrata sulla mappa");
+
+                } else {
+                    // Permessi Non Garantiti
+                    Log.d("Main Activity", "Permessi NON garantiti");
+
+                    myTextView.setText("Non hai dato i permessi per utilizzare la posizione. Non posso mostrare dove ti trovi");
+                }
+                return;
+            }
+
+        }
+    }
 }
