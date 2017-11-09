2c828e8f9d43fdd2a6aa88fb389e8bb4adc7f43f
cassanellicarlo <cassatek@gmail.com>
2017-11-09 13:36:07 +0100

    aggiunti tabs

---
+++ GeoPost/app/src/main/java/cassanellicarlo/geopost/ElencoAmici.java
@@ -0,0 +1,22 @@
+package cassanellicarlo.geopost;
+
+/**
+ * Created by carlo on 09/11/2017.
+ */
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ElencoAmici extends Fragment{
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.amici_elenco, container, false);
+        return rootView;
+    }
+
+}
--- GeoPost/app/src/main/java/cassanellicarlo/geopost/Login.java
+++ GeoPost/app/src/main/java/cassanellicarlo/geopost/Login.java
@@ -1,7 +1,9 @@
 package cassanellicarlo.geopost;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
@@ -17,7 +19,9 @@ import com.android.volley.toolbox.Volley;
 import java.util.HashMap;
 import java.util.Map;
 
-public class LoginActivity extends AppCompatActivity {
+public class Login extends AppCompatActivity {
+
+    public String session_id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +47,12 @@ public class LoginActivity extends AppCompatActivity {
                     public void onResponse(String response) {
                         // response
                         Log.d("Response", response); // Ritorna un Session ID
-                        String session_id=response;
+                        session_id=response;
+
+                        // Se torna un session id --> login corretto
+
+                        Intent intent = new Intent (getApplicationContext(),Amici.class);
+                        startActivity(intent);
                     }
                 },
                 new Response.ErrorListener()
